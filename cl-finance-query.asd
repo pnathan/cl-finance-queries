53374b685f1e6d0c7d741ea2c2be8b40a835cca7
pdn <pnathan@vandals.uidaho.edu>
2012-05-15 22:22:08 -0700

    Initial hack

---
+++ cl-finance-query.asd
@@ -0,0 +1,11 @@
+(asdf:defsystem #:cl-finance-query
+  :components ((:file "cl-finance-query"))
+  :name "cl-finance-query"
+  ;:version "dev"
+  :maintainer "Paul Nathan"
+  :author "Paul Nathan"
+  :license "LLGPL"
+  :description "Protocol for financial queries"
+  :long-description "Protocol for financial queries. Designed so that
+  various APIs can be shimmed into it such that users can trivially
+  instantiate the API and send it to cl-finance-query without pain")
---
+++ cl-finance-query.lisp
@@ -0,0 +1,27 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Protocol to provide an interface for financial queries
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;  An attempt to use generic functions/interface-passing style to
+;;;  provide a protocol suitable for cl-<service>-finance libraries.
+
+
+(defpackage :cl-finance-query
+  (:use :common-lisp)
+  (:export
+   :current-price
+   :historical-prices
+   :historical-splits
+   :company-immutables
+   :company-current-statistics
+   :company-historical-statistics))
+(in-package :cl-finance-query)
+
+(defun current-price (reader symbol-or-symbol-list &rest specifics)
+  "`reader` denotes a packages to execute the 'current-price' from"
+)
+(defun historical-prices (reader symbol-or-symbol-list start-date end-date &rest specifics))
+(defun historical-splits (reader symbol-or-symbol-list start-date end-date &rest specifics))
+(defun company-immutables (reader symbol-or-symbol-list  &rest specifics))
+(defun company-current-statistics (reader symbol-or-symbol-list  &rest specifics))
+(defun company-historical-statistics (reader symbol-or-symbol-list start-date end-date &rest specifics))
